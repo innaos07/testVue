b1f629967feef215dee10c6c125c4018091287ca
innaos07 <samosudova07@gmail.com>
2022-12-23 14:50:35 +0100

    added dist folder

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test-vue/',
+  base: '/testVue/',
   plugins: [vue()],
   resolve: {
     alias: {
